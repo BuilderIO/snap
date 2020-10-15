f348277538132b184bd4f195d087ec949916565d
Steve Sewell <steve@builder.io>
2020-10-15 08:41:27 -0700

    updates

--- packages/snap/build/scripts/build.ts
+++ packages/snap/build/scripts/build.ts
@@ -10,6 +10,9 @@ const cwd = process.cwd();
 // Generate code?
 
 export async function build() {
+  if (!process.env.NODE_ENV) {
+    process.env.NODE_ENV = 'production';
+  }
   const pages = await glob('pages/**/*');
 
   const compiler = webpack(
@@ -19,10 +22,24 @@ export async function build() {
       output: {
         path: path.join(cwd, 'dist', pagePath.split('.')[0]),
       },
+      alias: {
+        router: `
+          export default function AppRouter() {
+            return <Router>
+              ${pages
+                .map(
+                  () =>
+                    `<Route path=${path} component={lazy(() => import(${path}))}>`,
+                )
+                .join('\n')}
+            </Router>
+          }
+        `,
+      },
     })),
   );
 
   const run = promisify(compiler.run.bind(compiler));
   const stats = await run();
-  console.log(stats?.toString());
+  console.log(stats?.toString({}));
 }
